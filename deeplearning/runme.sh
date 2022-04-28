731230a3062286c1ddfd275fdd48105bfb9009f0
Yusuke Miyashita <58585405+Yusuke710@users.noreply.github.com>
2022-04-28 16:54:48 +1000

    Update runme.sh

--- deeplearning/runme.sh
+++ deeplearning/runme.sh
@@ -1,10 +1,9 @@
 module load python/3.6.2-static
-module load pytorch
 
 python3 -m venv .venv
 source .venv/bin/activate
 
 pip install --upgrade pip
-#pip install -r requirement.txt not necessary on massive
+pip install -r requirement.txt 
 
 python3 alexnet_stl10.py
